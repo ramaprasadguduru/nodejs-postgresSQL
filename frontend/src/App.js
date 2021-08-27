b63c6a83e5be915ced85589f4f073b91cb83cd10
Srihari <Srihari@zelarsoft.com>
2021-08-27 15:38:02 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
 
 
 class App extends Component {
