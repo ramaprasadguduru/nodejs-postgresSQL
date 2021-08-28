09201590cc223a9dec89d8bfc8a501c58d89b71a
Srihari <Srihari@zelarsoft.com>
2021-08-28 11:26:45 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,3 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-
-
-class App extends Component {
-  render() {
-    console.log("Host URL"+process.env.PUBLIC_URL);
-    return (
-
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
-        </header>
-      </div>
-    </Router>
-    );
-  }
-}
-
-export default App;
-
-
-
-
 const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
